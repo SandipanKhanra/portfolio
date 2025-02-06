1ff1f866f65219779f3971a2d444e1d8e8841e09
Sandipan Khanra <sandipankhanra@gmail.com>
2025-02-06 11:48:57 +0530

    Contact section added

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,34 @@
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:sandipankhanra@gmail.com">sandipankhanra@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/sandipankhanra/" target="_blank">
+            linkedin.com/in/sandipankhanra
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://www.github.com/myname" target="_blank">
+            github.com/myname
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
