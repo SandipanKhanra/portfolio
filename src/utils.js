2587cffc287de60ff5e496678dec1cbb7747c76a
Sandipan Khanra <sandipankhanra@gmail.com>
2025-02-06 11:55:21 +0530

    Styles modified in hero section

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
 export const getImageUrl = (path) => {
   console.log(import.meta.url);
-  return new URL(`/assets/${path}/`, import.meta.url).href;
+  return new URL(`/assets/${path}`, import.meta.url).href;
 };
