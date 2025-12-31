db9d56ffa376bc5997b2ba51dc775d7c82c02b8d
Sandipan Khanra <sandipankhanra@gmail.com>
2025-12-31 11:50:23 +0530

    Hero section updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,9 +7,10 @@ const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I&apos;m Sandipan</h1>
         <p className={styles.description}>
-          I&apos;m a Fullstack developer with 2+ years of experience building
-          responsive UI and Single-Page Applications in React, Angular and Java.
-          Reach out if you&apos;d like to know more!
+          Highly skilled and accomplished software developer with expertise in
+          Java, React, and Microservices. Demonstrated ability in driving
+          operational efficiency through automation and solution development,
+          leading to significant cost savings.
         </p>
         {/* <a href="mailto:sandipankhanra@gmail.com" className={styles.contactBtn}>
           Contact Me
